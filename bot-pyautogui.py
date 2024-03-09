6601cd3b4498bc70c593c8e4d3c228a8a77ec955
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-03-09 12:28:25 -0300

    script que abre e clica no login

---
+++ bot-pyautogui.py
@@ -0,0 +1,21 @@
+''' 
+O pyautogui é uma biblioteca que permite o controle do mouse e teclado do computador
+>pip install pyautogui
+'''
+
+# 1. abrir planilha / ferramenta / sistema / programa > tecla win + login.xlsx
+# 2. preencher login
+# 3. preencher senha
+# 4. clicar em fazer login
+
+import pyautogui
+
+pyautogui.PAUSE = 2 #Isso padroniza o tempo de espera antes da execução de cada comando, no caso, 2 segundos. Este seria o tempo de esperar o processamento dos comandos, pois como ele executa imediatamente, durante a automação pode ser que seja executado "um comando sobre outro", prejudicando o processo.
+
+#1.
+pyautogui.press("win") 
+pyautogui.write("Login.xlsx")
+pyautogui.press("enter")
+
+#2.
+pyautogui.click(x=434, y=213)
---
+++ bot-time.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+time.sleep(3)
+mouse_position = pyautogui.position()
+print("As coordenadas do cursor do mouse são:", mouse_position)
